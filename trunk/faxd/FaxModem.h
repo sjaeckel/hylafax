23ba7fb1218539d6526f89279528e74382485894
faxguy <faxguy@5505949e-d877-4686-9e03-c53b7a51b376>
2014-07-31 22:22:11 +0000

    This data type was missed in the updates libtiff v4.
    This was from Thomas Jarosch who posted at:
    
    http://bugs.hylafax.org/show_bug.cgi?id=958
    
    
    
    git-svn-id: https://svn.code.sf.net/p/hylafax/HylaFAX+@2347 5505949e-d877-4686-9e03-c53b7a51b376

--- trunk/faxd/FaxModem.h
+++ trunk/faxd/FaxModem.h
@@ -88,7 +88,7 @@ private:
     tstrip_t	recvStrip;	// current strip number during receive
     u_char*	recvRow;	// current receive row raster
     u_char*	recvPageStart;	// pointer to start of page buffer
-    u_long*	savedWriteOff;	// file offset at start of page data
+    tiff_offset_t*	savedWriteOff;	// file offset at start of page data
     TIFFSTRIPBYTECOUNTS*	savedStripByteCounts;	// byte counts at start of page data
     int		decodedPixels;	// number of pixels decoded with error
     int		decoderFd[2];	// file descriptors for the decoder pipe
